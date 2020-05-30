f7e5d5e8f869429221158afccc04734edd547b6c
AMcCall22 <alisonlmccallum@gmail.com>
2020-05-30 10:39:22 +0000

    Check for matches add, with placeholder functions for matched and unmatched outcomes.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 //Initialise variables.
 
-let clickedCard = null;
+let firstCard = null;
 let i = 0;
 
 //Create onCardClicked function to replace the facedown(rocket) image with the planet image.
 //onclick ="onCardClicked(event)" added to each planet div in index.html.
+//Reassign target to the card just clicked then increment i.
 function onCardClicked(planet) {
   let target = planet.currentTarget;
   target.className = target.className.replace("facedown", "");
   i++;
-}
+
+//Check for a match for each planet element, matching on "data-framework".
+//Point to matched or unmatched function 
+  if (i === 1) {
+    firstCard = target;
+  } else {
+    if (
+      firstCard.getAttribute("data-framework") ===
+      target.getAttribute("data-framework")
+    ) {
+      console.log("has been matched");
+      matched();
+    } else {
+      console.log("has not been matched");
+      unmatched();
+    }
+    i = 0;
+  }}
+
